c955d16ec236be8b04c669ff008be00ceb8dde27
SheikhSalman <assalmanmuhammad@gmail.com>
2026-02-24 15:37:56 +0600

    error and loading of about page

---
+++ app/about/error.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { Button } from '@/components/ui/button';
+import React, { useEffect } from 'react'
+
+export default function Error({error, reset}: {error: Error & {digest?: string}, reset: ()=> void}) {
+
+    useEffect(()=>{
+        console.error(error);
+    },[error])
+
+  return (
+    <div>
+
+        <h2>Something went wrong, please try again later</h2>
+        <Button
+        onClick={() => reset()}
+        >
+            Retry
+        </Button>
+    </div>
+  )
+}
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,12 @@
+
 import React from 'react'
 
-export default function AboutPage() {
+export default async function AboutPage() {
+
+  await new Promise((resolve)=> setTimeout(resolve, 4000))
+
+  throw new Error('something went wrong!')
+
   return (
     <div>this is about page</div>
   )
